dd827d27eda4e12f4cfd3216b46fd00e0a55caba
wassimans <wassim@wassimans.com>
2025-05-22 19:56:46 +0200

    Add iOS, Android projects

---
+++ ios/speem/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  speem
+//
+//  Created by Wassim Mansouri on 22/05/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios/speem/speemApp.swift
@@ -0,0 +1,17 @@
+//
+//  speemApp.swift
+//  speem
+//
+//  Created by Wassim Mansouri on 22/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct speemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
